053f23206be8842e424547f1a94325c481a3f9ee
Manel <125216716+menelo25@users.noreply.github.com>
2023-11-08 15:44:38 -0500

    Q8

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import click
 
+__version__ = "0.1.1"
+
+@click.command("guten-tag")
+def bonjour():
+    click.echo("guten tag")
+
+@click.command("version")
+def version():
+    click.echo(__version__)
+
 @click.command("hello")
 def hello():
     click.echo("Hello, World!")
